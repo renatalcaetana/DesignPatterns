fe584bd59b528a858205a4fa8a4e91b039eb8b85
Renata <renatalcaetana@gmail.com>
2024-09-02 21:17:30 -0300

    sha512

---
+++ CriptografiaSha512/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CriptografiaSha512;
+
+Console.WriteLine("Hello, World!");
+
+
+var Criptografia = new Sha512();
+var key =  Criptografia.Criptografar("renatalcaetana@gmail.com");
+Console.WriteLine($"Valor Criptografado {key}");
---
+++ CriptografiaSha512/Sha512.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace CriptografiaSha512
+{
+    public class Sha512
+    {
+        public string Criptografar(string valor)
+        {
+
+
+            // Converter a string de entrada em um array de bytes
+            byte[] bytes = Encoding.UTF8.GetBytes(valor);
+
+            // Criar uma instância de SHA-512
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                // Gerar o hash
+                byte[] hashBytes = sha512.ComputeHash(bytes);
+
+                // Converter o hash em uma string hexadecimal
+                StringBuilder hashString = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    hashString.Append(b.ToString("x2"));
+                }
+
+                return hashString.ToString();
+
+            }
+        }
+
+    }
+}
